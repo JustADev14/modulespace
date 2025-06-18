8c3c667dc589f91d0269e1516a90a988b04195a5
Brandon Martin <brandon.martin@Mac.hsd1.ut.comcast.net>
2025-06-18 12:43:32 -0600

    Remove all React code and dependencies; Vue-only project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/**/*.{vue,js,ts}',
   ],
   theme: {
     extend: {
